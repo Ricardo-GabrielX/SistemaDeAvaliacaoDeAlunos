307d32ac87d2b58304be35af5942ac3879e16110
Gabriel <gabrieloliveira280807@gmail.com>
2025-06-02 21:24:49 -0300

    :sparkles: feat: ajustando telas para melhor visualização, botão PFD funcionando agora, e botão de excluir também

--- SistemaDeAvaliacaoDeAlunos/Controllers/AlunoController.cs
+++ SistemaDeAvaliacaoDeAlunos/Controllers/AlunoController.cs
@@ -14,6 +14,7 @@ using System.Drawing.Printing;
 using System.Drawing;
 using System.Xml.Linq;
 using SistemaDeAvaliacaoDeAlunos.Models;
+using iTextSharp.text.pdf.draw;
 
 
 namespace WebApplication1.Controllers
@@ -130,115 +131,98 @@ namespace WebApplication1.Controllers
         }
 
 
-        //public ActionResult GerarPdf()
-        //{
-        //    List<Aluno> listaAlunos = Session["ListaAluno"] as List<Aluno>;
-
-        //    if (listaAlunos == null || !listaAlunos.Any())
-        //    {
-        //        return Content("Nenhum aluno encontrado para gerar o PDF");
-        //    }
-        //    using (MemoryStream memoryStream = new MemoryStream())
-        //    {
-        //        Document document = new Document(PageSize.A4, 50, 50, 25, 25);
-
-        //        try
-        //        {
-        //            PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
-        //            document.Open();
-
-        //            // Configuração de fontes
-        //            Font titleFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 18);
-        //            Font headerFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12);
-        //            Font normalFont = FontFactory.GetFont(FontFactory.HELVETICA, 10);
-
-        //            // Título
-        //            document.Add(new Paragraph("Lista de Alunos", titleFont));
-        //            document.Add(Chunk.NEWLINE);
-
-        //            // Tabela
-        //            PdfPTable table = new PdfPTable(3);
-        //            table.WidthPercentage = 100;
-
-        //            // Cabeçalhos
-        //            table.AddCell(new Phrase("Nome", headerFont));
-        //            table.AddCell(new Phrase("RA", headerFont));
-        //            table.AddCell(new Phrase("Data", headerFont));
-
-        //            // Dados
-        //            foreach (var aluno in listaAlunos)
-        //            {
-        //                table.AddCell(new Phrase(aluno.Nome ?? "", normalFont));
-        //                table.AddCell(new Phrase(aluno.RA ?? "", normalFont));
-
-        //                string dataFormatada = aluno.Data.ToString("dd/MM/yyyy");
-        //                table.AddCell(new Phrase(dataFormatada, normalFont));
-        //            }
-
-        //            document.Add(table);
-        //        }
-        //        finally
-        //        {
-        //            if (document.IsOpen())
-        //            {
-        //                document.Close();
-        //            }
-        //        }
-
-        //        return File(memoryStream.ToArray(), "application/pdf", "ListaAlunos.pdf");
-        //    }
-        //}
-
-        public ActionResult GerarExcel()
+        public ActionResult GerarPdf()
         {
-            var lista = Session["ListaAluno"] as List<Aluno>;
+            List<Aluno> listaAlunos = Session["ListaAluno"] as List<Aluno>;
 
-            if (lista == null || !lista.Any())
-                return RedirectToAction("Listar");
-
-            ExcelPackage.License.SetNonCommercialOrganization("ETEC Fernando Prestes extensão Fatec");
-
-            using (var pacote = new ExcelPackage())
+            if (listaAlunos == null || !listaAlunos.Any())
             {
-                var planilha = pacote.Workbook.Worksheets.Add("Alunos");
+                return Content("Nenhum aluno encontrado para gerar o PDF");
+            }
 
-                // Cabeçalho
-                planilha.Cells[1, 1].Value = "Nome";
-                planilha.Cells[1, 2].Value = "RA";
-                planilha.Cells[1, 3].Value = "Data de Nascimento";
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                Document document = new Document(PageSize.A4, 50, 50, 25, 25);
 
-                using (var faixaCabecalho = planilha.Cells[1, 1, 1, 3])
+                try
                 {
-                    faixaCabecalho.Style.Font.Bold = true;
-                    faixaCabecalho.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
-                    faixaCabecalho.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGreen);
-                    faixaCabecalho.Style.Font.Color.SetColor(System.Drawing.Color.Black);
-                    faixaCabecalho.Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                    PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
+                    document.Open();
+
+                    // Fontes
+                    var titleFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 18);
+                    var headerFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12);
+                    var subHeaderFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 11);
+                    var normalFont = FontFactory.GetFont(FontFactory.HELVETICA, 10);
+
+                    document.Add(new Paragraph("📚 Lista de Alunos com Notas", titleFont));
+                    document.Add(Chunk.NEWLINE);
+
+                    foreach (var aluno in listaAlunos)
+                    {
+                        // Informações do aluno
+                        PdfPTable infoTable = new PdfPTable(3);
+                        infoTable.WidthPercentage = 100;
+                        infoTable.SpacingAfter = 10;
+
+                        infoTable.AddCell(new Phrase("Nome:", headerFont));
+                        infoTable.AddCell(new Phrase("RA:", headerFont));
+                        infoTable.AddCell(new Phrase("Data de Nascimento:", headerFont));
+
+                        infoTable.AddCell(new Phrase(aluno.Nome ?? "", normalFont));
+                        infoTable.AddCell(new Phrase(aluno.RA ?? "", normalFont));
+                        infoTable.AddCell(new Phrase(aluno.Data.ToString("dd/MM/yyyy"), normalFont));
+
+                        document.Add(infoTable);
+
+                        // Tabela de Notas
+                        PdfPTable notaTable = new PdfPTable(6);
+                        notaTable.WidthPercentage = 100;
+
+                        // Cabeçalhos
+                        notaTable.AddCell(new Phrase("Disciplina", subHeaderFont));
+                        notaTable.AddCell(new Phrase("Bim. 1", subHeaderFont));
+                        notaTable.AddCell(new Phrase("Bim. 2", subHeaderFont));
+                        notaTable.AddCell(new Phrase("Bim. 3", subHeaderFont));
+                        notaTable.AddCell(new Phrase("Bim. 4", subHeaderFont));
+                        notaTable.AddCell(new Phrase("Média", subHeaderFont));
+
+                        if (aluno.Notas != null && aluno.Notas.Any())
+                        {
+                            foreach (var nota in aluno.Notas)
+                            {
+                                notaTable.AddCell(new Phrase(nota.Disciplina?.Nome ?? "", normalFont));
+                                notaTable.AddCell(new Phrase(nota.Bimestre1.ToString("F1"), normalFont));
+                                notaTable.AddCell(new Phrase(nota.Bimestre2.ToString("F1"), normalFont));
+                                notaTable.AddCell(new Phrase(nota.Bimestre3.ToString("F1"), normalFont));
+                                notaTable.AddCell(new Phrase(nota.Bimestre4.ToString("F1"), normalFont));
+                                notaTable.AddCell(new Phrase(nota.Media.ToString("F2"), normalFont));
+                            }
+                        }
+                        else
+                        {
+                            PdfPCell cell = new PdfPCell(new Phrase("Nenhuma nota cadastrada", normalFont));
+                            cell.Colspan = 6;
+                            cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                            notaTable.AddCell(cell);
+                        }
+
+                        document.Add(notaTable);
+
+                        // Status geral do aluno
+                        document.Add(new Paragraph($"Status: {aluno.Status}", normalFont));
+                        document.Add(new Paragraph(" "));
+                        document.Add(new LineSeparator(1f, 100f, BaseColor.LIGHT_GRAY, Element.ALIGN_CENTER, -2));
+                        document.Add(new Paragraph(" "));
+                    }
+                }
+                finally
+                {
+                    if (document.IsOpen())
+                        document.Close();
                 }
 
-                // Dados
-                for (int i = 0; i < lista.Count; i++)
-                {
-                    var aluno = lista[i];
-                    planilha.Cells[i + 2, 1].Value = aluno.Nome;
-                    planilha.Cells[i + 2, 2].Value = aluno.RA;
-                    planilha.Cells[i + 2, 3].Value = aluno.Data.ToString("dd/MM/yyyy");
-                }
-
-                planilha.Cells.AutoFitColumns();
-
-                // Bordas nas células preenchidas
-                var faixaDados = planilha.Cells[1, 1, lista.Count + 1, 3];
-                faixaDados.Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
-                faixaDados.Style.Border.Bottom.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
-                faixaDados.Style.Border.Left.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
-                faixaDados.Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
-
-                return File(
-                    pacote.GetAsByteArray(),
-                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                    "Alunos.xlsx"
-                );
+                return File(memoryStream.ToArray(), "application/pdf", "ListaCompletaAlunos.pdf");
             }
         }
 
